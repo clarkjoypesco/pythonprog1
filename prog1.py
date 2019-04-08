e085dda4697784862aae74641faedf9117f38b5b
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-09 07:25:00 +0800

    initial commit

---
+++ prog1.py
@@ -0,0 +1,9 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+hours_in_day = 24
+days_in_week = 7
+weeks = 7
+
+
+hours_in_weeks = hours_in_day * days_in_week * weeks
+
+print hours_in_weeks
